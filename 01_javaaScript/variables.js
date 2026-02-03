09e9658867356b14357689652598856a0d9f234c
Aayush Vishwakarma <aayushvish0047@gmail.com>
2026-02-03 11:30:04 +0530

    hey

---
+++ 01_javaaScript/variables.js
@@ -0,0 +1,14 @@
+const accountId=14553
+let accountEmail="aayushvish0047@gmail.com"
+var accountPassword="12345"
+acountCity="jaipur"
+
+// accountId= 2
+accountEmail="hey@123.com"
+accountPassword="6789"
+acountCity="Mumbai"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(acountCity);
