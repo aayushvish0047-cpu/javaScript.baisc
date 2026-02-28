62cf7e54006180fe7d503519f9646e0622590f43
Aayush Vishwakarma <aayushvish0047@gmail.com>
2026-02-28 17:04:29 +0530

    Update variables.js

--- 01_javaaScript/variables.js
+++ 01_javaaScript/variables.js
@@ -3,7 +3,7 @@ let accountEmail="aayushvish0047@gmail.com"
 var accountPassword="12345"
 acountCity="jaipur"
 
-// accountId= 2
+accountId= 2
 accountEmail="hey@123.com"
 accountPassword="6789"
 acountCity="Mumbai"
