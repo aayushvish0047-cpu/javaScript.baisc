bdf68db84c39e288ec29ae3182027b5db76c79fd
Aayush vishwakrma <aayushvish0047@gmail.com>
2026-03-13 17:14:00 +0000

    add data type file

---
+++ 01_javaaScript/DataType.js
@@ -0,0 +1,16 @@
+let LuckyNumber=47
+let Name="Aayush Vishwakarma"
+let isLoggedIn=true
+let state=undefined
+let empty=null
+console.log(LuckyNumber);
+console.log(typeof LuckyNumber);
+let  myluckyNumber=String(LuckyNumber);
+console.log(myluckyNumber);
+console.log(typeof myluckyNumber);
+// if we want to convert a data type to another we have to use sytax like let variableName=DataType(variable);
+console.log(typeof Name);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof empty);
+// we can also use typeof operator to check the data type of a variable and it will return the data type of the variable as a string
