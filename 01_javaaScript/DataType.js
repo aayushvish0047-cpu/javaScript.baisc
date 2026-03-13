c11e493a1701541ec6ccef7758f980b2179454a5
Aayush vishwakrma <aayushvish0047@gmail.com>
2026-03-13 18:51:45 +0000

    edit in my data type file

--- 01_javaaScript/DataType.js
+++ 01_javaaScript/DataType.js
@@ -1,16 +1,30 @@
-let LuckyNumber=47
-let Name="Aayush Vishwakarma"
-let isLoggedIn=true
-let state=undefined
-let empty=null
-console.log(LuckyNumber);
-console.log(typeof LuckyNumber);
-let  myluckyNumber=String(LuckyNumber);
-console.log(myluckyNumber);
-console.log(typeof myluckyNumber);
-// if we want to convert a data type to another we have to use sytax like let variableName=DataType(variable);
-console.log(typeof Name);
-console.log(typeof isLoggedIn);
-console.log(typeof state);
-console.log(typeof empty);
+// let LuckyNumber=47
+// let Name="Aayush Vishwakarma"
+// let isLoggedIn=true
+// let state=undefined
+// let empty=null
+// console.log(LuckyNumber);
+// console.log(typeof LuckyNumber);
+// let  myluckyNumber=String(LuckyNumber);
+// console.log(myluckyNumber);
+// console.log(typeof myluckyNumber);
+// console.log(typeof Name);
+// console.log(typeof isLoggedIn);
+// console.log(typeof state);
+// console.log(typeof empty);
+
 // we can also use typeof operator to check the data type of a variable and it will return the data type of the variable as a string
+// if we want to convert a data type to another we have to use sytax like let variableName=DataType(variable);
+let code="123"
+
+// console.log(typeof code)
+// let password=Number(code)
+// console.log(password)
+// console.log(typeof password)
+let isRegistered=1
+console.log(isRegistered)
+console.log(typeof isRegistered)
+isRegistered=Boolean(isRegistered)
+console.log(isRegistered)
+console.log(typeof isRegistered)
+console.table([isRegistered,typeof isRegistered])
