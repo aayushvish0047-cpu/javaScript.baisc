624986abba8b0576c9673a378a3f00f0e6d3ed65
Aayush vishwakrma <aayushvish0047@gmail.com>
2026-03-15 08:37:11 +0000

    add comaparsion file

---
+++ 01_javaaScript/comaparesion.js
@@ -0,0 +1,13 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log(1<"4");
+// console.log("aayush"<9);
+//  avoid this type of compareison@
+// /strictly check  ===
+console.log("2"===2);
+
