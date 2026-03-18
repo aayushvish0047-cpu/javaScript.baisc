35c11d65b14cab007668a7ffe242d0038a1afe51
Aayush vishwakrma <aayushvish0047@gmail.com>
2026-03-18 15:03:11 +0000

    some change in string file

--- 01_javaaScript/string.js
+++ 01_javaaScript/string.js
@@ -2,5 +2,13 @@
 const myName= 'Aayush';
 const classYear=1
 const whturStuding= "Enginerring"
-console.log("My name is "${myName}"im in" ${classYear} 'currently im doing' ${whturStuding})
-    
+// console.log(`My name is ${myName} im in ${classYear} currently im doing ${whturStuding}`);
+
+const newName =String("Aayush")
+console.log(newName[0]);
+console.log(newName.__proto__);
+console.log(newName.length);
+console.log(newName.toUpperCase);
+console.log(newName.charCodeAt(4));
+console.log(newName.indexOf('y'));
+
