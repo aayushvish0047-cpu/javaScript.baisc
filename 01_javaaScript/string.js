676a16e56453a2a4489e063e73432775e855b2aa
Aayush vishwakrma <aayushvish0047@gmail.com>
2026-03-26 15:32:16 +0000

    kuch bhii nhi

--- 01_javaaScript/string.js
+++ 01_javaaScript/string.js
@@ -5,10 +5,15 @@ const whturStuding= "Enginerring"
 // console.log(`My name is ${myName} im in ${classYear} currently im doing ${whturStuding}`);
 
 const newName =String("Aayush")
-console.log(newName[0]);
-console.log(newName.__proto__);
-console.log(newName.length);
-console.log(newName.toUpperCase);
-console.log(newName.charCodeAt(4));
-console.log(newName.indexOf('y'));
+// console.log(newName[0]);
+// console.log(newName.__proto__);
+// console.log(newName.length);
+// console.log(newName.toUpperCase);
+// console.log(newName.charCodeAt(4));
+// console.log(newName.indexOf('y'));// y konse se index pe hai 
+// console.log(newName.charAt(3));//index 3 pe konsa leter hai 
+newString = myName.substring(0,4)
+console.log(newString)
+anotherString = myName.slice(-6,4)
+console.log(anotherString)
 
